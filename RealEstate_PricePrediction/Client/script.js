9245138f280500b51afa06fd3378f85c304e81c2
Aditya Solanki <aditya2ss283@gmail.com>
2021-12-21 20:56:38 +0530

    Update nginx dynamic calls

--- RealEstate_PricePrediction/Client/script.js
+++ RealEstate_PricePrediction/Client/script.js
@@ -5,7 +5,7 @@ function getBathValue() {
             return parseInt(i) + 1;
         }
     }
-    return -1; // Invalid Value
+    return -1;
 }
 
 function getBHKValue() {
@@ -15,7 +15,7 @@ function getBHKValue() {
             return parseInt(i) + 1;
         }
     }
-    return -1; // Invalid Value
+    return -1;
 }
 
 function onClickedEstimatePrice() {
@@ -26,9 +26,8 @@ function onClickedEstimatePrice() {
     var location = document.getElementById("uiLocations");
     var estPrice = document.getElementById("uiEstimatedPrice");
 
-    var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are
-    // NOT using nginx which is first 7 tutorials
-    // var url = "/api/predict_home_price"; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+    //var url = "http://127.0.0.1:5000/predict_home_price"; //local
+    var url = "/api/predict_home_price"; // Nginx integrated
 
     $.post(url, {
         total_sqft: parseFloat(sqft.value),
@@ -46,7 +45,8 @@ function onClickedEstimatePrice() {
 
 function onPageLoad() {
     console.log("Locations are Loaded");
-    var url = "http://127.0.0.1:5000/get_location_names";
+    //var url = "http://127.0.0.1:5000/get_location_names";
+    var url = "/api/get_location_names"; //using Nginx
     $.get(url, function (data, status) {
         console.log("got response for get_location_names request");
         if (data) {
