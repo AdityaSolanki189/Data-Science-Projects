0e8f5b28c46f70bd4fad6af27c3043b8de740c0a
Aditya Solanki <aditya2ss283@gmail.com>
2022-05-18 13:51:27 +0530

    remove

--- Misc_Codes/WTL/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$database = 'localhost';
-$dbName = 'mitadt';
-$dbUser = 'root';
-$dbPass = 'amit';
-$mysqli = mysqli_connect($database, $dbUser, $dbPass, $dbName);
-?>
--- Misc_Codes/WTL/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-include("config.php");
-$id = $_GET['id'];
-$result = mysqli_query($mysqli,"DELETE FROM users WHERE id=$id");
-header("Location:index.php");
-?>
